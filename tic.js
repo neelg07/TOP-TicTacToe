a4457c90d0b2df8a5f34e7acef7be10d56773e12
neelg07 <ineel7762@gmail.com>
2023-01-26 21:57:57 -0500

    Add responsiveness to player gamemode selection; Includes text appear/hide on hover func

---
+++ tic.js
@@ -0,0 +1,26 @@
+// Show versus-text depending on where user is hovering
+const pvp = document.querySelector('.pvp img');
+const pvai = document.querySelector('.ai img');
+const pvpText = document.getElementById('pvp-txt');
+const pvaiText = document.getElementById('pvai-txt');
+
+// Toggle class with display:none;
+function toggleHidden(query) {
+    query.classList.toggle('hidden');
+}
+
+pvp.addEventListener('mouseover', () => {
+    toggleHidden(pvpText);
+});
+
+pvp.addEventListener('mouseout', () => {
+    toggleHidden(pvpText);
+});
+
+pvai.addEventListener('mouseover', () => {
+    toggleHidden(pvaiText);
+});
+
+pvai.addEventListener('mouseout', () => {
+    toggleHidden(pvaiText);
+});
